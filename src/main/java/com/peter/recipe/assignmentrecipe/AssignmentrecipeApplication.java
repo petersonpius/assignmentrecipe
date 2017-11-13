1feec000362491a429dfd95c619201cde56fb2c6
pc72451 <peterson.chiramel@jetblue.com>
2017-11-12 21:03:42 -0500

    Recipe creator

---
+++ src/main/java/com/peter/recipe/assignmentrecipe/AssignmentrecipeApplication.java
@@ -0,0 +1,12 @@
+package com.peter.recipe.assignmentrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentrecipeApplication.class, args);
+	}
+}
